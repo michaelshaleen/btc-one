60797b4d0cf6a3f848202bb429e493a5cff26987
michaelshaleen <shaleenmichael@yahoo.com>
2021-12-25 12:45:12 -0600

    arrow rotation with nav drop down ternary

--- src/components/User_Profile/User_Profile.jsx
+++ src/components/User_Profile/User_Profile.jsx
@@ -1,15 +1,24 @@
 import './User_Profile.css';
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 import Button from '@material-ui/core/Button';
 
 
 function User_Profile() {
+  const [nav, setNav] = useState(true);
+  const handleNav = () => setNav(!nav);
+  console.log(nav, "nav here");
+
+
+
   return (<>
-  <nav className="main-nav">
-    <span className="arrow-down">
-      <KeyboardDoubleArrowDownIcon />
+  <nav className={nav ? "main-nav" : "main-nav-long"}>
+    <span className="arrow-down" onClick={handleNav}>
+      <KeyboardDoubleArrowDownIcon className={nav ?  "arrow-up" : "arrow-down"} />
+      {/* <i className={nav ? "fa fa-arrow-circle-up" : "fa fa-arrow-circle-o-down"} aria-hidden="true"></i> */}
+
     </span>
     BTC Buds
     <span>
